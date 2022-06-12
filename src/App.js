cffc8c615e7f84c47d55895c44a752e7b2eef0ee
plaxidia <plaxidiahove@gmail.com>
2022-06-12 23:58:29 +0300

    created a new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React ,{ Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet'
 //import {Greet }from './Components/Greet'
+import Welcome from './Components/welcome';
+
 function App() {
+  
   return (
     <div className="App">
      <Greet></Greet>
+     
+      <Welcome> </Welcome>
     </div>
   );
+  
 }
 
 export default App;
